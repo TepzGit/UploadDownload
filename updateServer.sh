b398b08d79fa3d9b51c77eed58c30b9243654b54
Tepz <Tepz@Tepz.com>
2026-05-16 08:01:30 +0200

    Update

--- updateServer.sh
+++ updateServer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-scp -r assets css html js UploadDownload.go Server:UploadDownload/.
+scp -r * Server:UploadDownload/.
 ssh -t Server "cd UploadDownload && sed -i 's/port := 8000/port := 42067/' UploadDownload.go && go build UploadDownload.go && sudo systemctl restart UploadDownloader"
